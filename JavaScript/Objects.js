f09cfcb3ce9182c97eaa066405275807e1c689c6
Ved <Vedvishe32@gmail.com>
2026-08-19 10:57:00 +0530

    creation of function in object

--- JavaScript/Objects.js
+++ JavaScript/Objects.js
@@ -1,64 +1,84 @@
-//  object creation 
+// //  object creation 
 
-const person ={
-    name: "ved",
-    age:20,
-    gender:"male",
-    email :"ved@gmail.com"
-}
+// const person ={
+//     name: "ved",
+//     age:20,
+//     gender:"male",
+//     email :"ved@gmail.com"
+// }
 
-console.log( typeof person);
+// console.log( typeof person);
 
-console.log(person.name);
+// console.log(person.name);
 
-//  CRUD operation : create ,read,,update, delete
+// //  CRUD operation : create ,read,,update, delete
 
-// create
-person.city="shahapur";
-person.salary= 50000;
+// // create
+// person.city="shahapur";
+// person.salary= 50000;
 
-console.log(person);
-
-
-// read
-console.log(person.salary);
-console.log(person.email);
+// console.log(person);
 
 
-// update
+// // read
+// console.log(person.salary);
+// console.log(person.email);
 
-person.salary=100000;
-person.city="kinhavali";
-console.log(person.salary);
-console.log(person.city);
 
-// delete
+// // update
+
+// person.salary=100000;
+// person.city="kinhavali";
+// console.log(person.salary);
+// console.log(person.city);
+
+// // delete
  
-delete person.salary;
-delete person.age;
-console.log(person);
+// delete person.salary;
+// delete person.age;
+// console.log(person);
 
 
-const user2 = person;
-user2.age=21;
-console.log(person);
+// const user2 = person;
+// user2.age=21;
+// console.log(person);
 
 
-console.log(Object.keys(person));
-console.log(Object.values(user2));
+// console.log(Object.keys(person));
+// console.log(Object.values(user2));
 
-for(keys in person){
-    console.log(keys);
-    console.log(keys +":"+[person[keys]]);
+// for(keys in person){
+//     console.log(keys);
+//     console.log(keys +":"+[person[keys]]);
+// }
+
+
+// // const name =person.name;
+// // const gender = person.gender;
+// // console.log(name,gender);
+// //  write this in a standerd way like this:
+
+// // object destructuring
+// const {name,gender,email}= person;
+// console.log(name,gender,email);
+
+
+// you can also create a function in obejct 
+const person={
+    name:"ved",
+    age:21,
+    city:"shahapur",
+    email:"ved@gmail.com",
+
+    hello: function(){
+        console.log("hello my name is"+this.name);
+        return this.age;
+    }
 }
 
 
-// const name =person.name;
-// const gender = person.gender;
-// console.log(name,gender);
-//  write this in a standerd way like this:
+const result = person.hello();
+console.log(result);
 
-// object destructuring
-const {name,gender,email}= person;
-console.log(name,gender,email);
+
 
